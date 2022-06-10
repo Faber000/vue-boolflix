56e5f5c1785a3c789a092d1d68191c6d2f8a41df
Fabio Moro <fabio.moro2000@gmail.com>
2022-06-10 18:52:34 +0200

    Milestone 2

--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -1,24 +1,43 @@
 <template>
-    <header>
+    <header class="header">
         <form @submit.prevent="searching">
             <input type="text" v-model="searchText">
             <button>Search</button>
         </form>
-        <div class="film" v-for="movie in shared.SharedFilms" :key="movie.id">
-            <div>
-                Titolo: {{movie.title}}
+        <div class="results">
+
+            <h1>Film</h1>
+            <div class="film" v-for="movie in shared.SharedFilms" :key="movie.id">
+                <div>
+                    Titolo: {{movie.title}}
+                </div>
+                <div>
+                    Titolo originale: {{movie.original_title}}
+                </div>
+                <div>
+                    <lang-flag :iso="(movie.original_language)"/>
+                </div>
+                <div>
+                    Voto: {{movie.vote_average}}
+                </div>
             </div>
-            <div>
-                Titolo originale: {{movie.original_title}}
+
+            <h1>Serie Tv</h1>
+            <div class="serie" v-for="serie in shared.SharedtvSeries" :key="serie.id">
+                <div>
+                    Titolo: {{serie.name}}
+                </div>
+                <div>
+                    Titolo originale: {{serie.original_name}}
+                </div>
+                <div>
+                    <lang-flag :iso="(serie.original_language)"/>
+                </div>
+                <div>
+                    Voto: {{serie.vote_average}}
+                </div>
             </div>
-            <div>
-                Lingua: {{movie.original_language}}
-            </div>
-            <div>
-                Voto: {{movie.vote_average}}
-            </div>
-        </div>
-        
+        </div> 
     </header>
 </template>
 
@@ -50,6 +69,20 @@ export default {
                 }).catch((error) => {
                     console.log(error);
                 })
+
+                axios.get('https://api.themoviedb.org/3/search/tv',
+                    {
+                        params: {
+                            api_key: 'ae54bc9c4c06af63c995413eb44a9dac',
+                            query: this.searchText,
+                            language: 'it-IT'
+                        }
+                    }
+                ).then((response) => {
+                    shared.SharedtvSeries = response.data.results;
+                }).catch((error) => {
+                    console.log(error);
+                })
         }
     }
 }
@@ -57,7 +90,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.film {
+.header {
     margin: 20px;
+}
+.film, .serie {
+    margin-bottom: 30px;
+}
+
+.film > *, .serie > * {
+    margin-bottom: 10px;
 }
 </style>
